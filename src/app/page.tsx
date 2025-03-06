409143997aa7d81106bb5f82634da41c2c3727d4
호예준 <hyjcompany30@naver.com>
2025-03-06 16:07:22 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,23 @@ import {PricingTable} from "@/components/pricing-table";
 
 const features = [
     {name: "Platform Access", included: "free"},
+
     {name: "HD Video", included: "free"},
     {name: "4K Video", included: "starter"},
+
     {name: "Observe Streamers", included: "free"},
+
     {name: "Keep video permanently ($0.2 per video)", included: "starter"},
+
     {name: "Unlimited Dance Cut Downloads", included: "starter"},
     {name: "Unlimited Dance Cut Streaming", included: "starter"},
+
     {name: "5 Video Downloads", included: "free", excluded: "starter"},
     {name: "Unlimited Video Downloads", included: "starter"},
-    {name: "10 Hours Video Streaming", included: "free", excluded: "pro"},
+
+    {name: "10 Hours Video Streaming", included: "free", excluded: "starter"},
     {name: "Unlimited Video Streaming", included: "starter"},
+
     {name: "Last 30 Day 720p Videos", included: "free", excluded: "starter"},
     {name: "Last 90 Day 720p Videos", included: "starter"},
     {name: "Last 90 Day 4K Videos", included: "starter"}
